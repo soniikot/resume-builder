dc47aef75ea5de2bb4e618a45cf8610e8ea06813
soniikot <soniikot@gmail.com>
2024-08-01 15:39:11 -0700

    possible solution to add multiple jobs

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,4 +1,6 @@
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import "./Experience.css";
+import { useState } from "react";
 
 const JOB_EXPERIENCE = [
   "jobTitle",
@@ -11,48 +13,63 @@ export default function Experience() {
 
   const { jobTitle, dateStarted, dateFinished, description } = values;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleFormChange = (index, event) => {
+    let data = [...values];
+    data[index][event.target.name] = event.target.value;
+    setValues(data);
+  };
 
-    setValues(name, value);
+  const addJobExperience = () => {
+    let newJob = { jobTitle: "", dateStarted: "" };
+    setValues([...values, newJob]);
   };
 
   return (
     <div id="2">
       <h2>Experience</h2>
-      <form className="form">
-        <label htmlFor="jobTitle">Job Title</label>
-        <input
-          type="text"
-          name="jobTitle"
-          value={jobTitle}
-          onChange={handleChange}
-        />
+      {values.map((index) => {
+        return (
+          <>
+            <h3>Job</h3>
+            <form className="form" key={index}>
+              <label htmlFor="jobTitle`${">Job Title</label>
+              <input
+                type="text"
+                name="jobTitle"
+                value={jobTitle}
+                onChange={(event) => handleFormChange(index, event)}
+              />
 
-        <label htmlFor="dateStarted">Date started</label>
-        <input
-          type="date"
-          name="dateStarted"
-          value={dateStarted}
-          onChange={handleChange}
-        />
+              <label htmlFor="dateStarted">Date started</label>
+              <input
+                type="date"
+                name="dateStarted"
+                value={dateStarted}
+                onChange={(event) => handleFormChange(index, event)}
+              />
 
-        <label htmlFor="dateFinished">Date finished</label>
-        <input
-          type="date"
-          name="dateFinished"
-          value={dateFinished}
-          onChange={handleChange}
-        />
+              <label htmlFor="dateFinished">Date finished</label>
+              <input
+                type="date"
+                name="dateFinished"
+                value={dateFinished}
+                onChange={(event) => handleFormChange(index, event)}
+              />
 
-        <label htmlFor="description">Responsibiliries </label>
-        <input
-          type="textarea"
-          name="description"
-          value={description}
-          onChange={handleChange}
-        />
-      </form>
+              <label htmlFor="description">Responsibilities </label>
+              <input
+                type="textarea"
+                name="description"
+                value={description}
+                onChange={(event) => handleFormChange(index, event)}
+              />
+            </form>
+          </>
+        );
+      })}
+      <button className="addButton" onClick={addJobExperience}>
+        Add another Job
+      </button>
     </div>
   );
 }
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,29 +1,25 @@
 import { useState, useEffect } from "react";
 
-export function useLocalStorage(fields) {
-  const [values, setValues] = useState({});
+export function useLocalStorage(fields, initialValues = []) {
+  const [values, setValues] = useState(initialValues);
 
   const setInnerValue = (name, value) => {
     localStorage.setItem(name, value);
-    setValues((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setValues((prev) => {
+      const newValues = [...prev];
+      newValues[newValues.findIndex((v) => v.name === name)] = { name, value };
+      return newValues;
+    });
   };
 
   useEffect(() => {
-    const newValues = {};
-
     fields.forEach((name) => {
       const savedField = localStorage.getItem(name);
 
       if (savedField) {
-        newValues[name] = savedField;
+        const initialValue = { name, value: savedField };
+        setValues((prev) => [...prev, initialValue]);
       }
-    });
-
-    setValues((prev) => {
-      return { ...prev, ...newValues };
     });
   }, [fields]);
 

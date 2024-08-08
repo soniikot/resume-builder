3c95a841d40fa1210820db1ca7b8876e7238f2a5
soniikot <soniikot@gmail.com>
2024-08-08 12:16:10 -0700

    finish forms

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,72 @@
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const BLANK_PROJECT = {
+  id: null,
+  title: "",
+  link: "",
+  description: "",
+};
+
 export default function Projects() {
+  const [projects, setProjects, setFieldList] = useLocalStorage("projects");
+
+  const handleClickAdd = () => {
+    Object.entries(BLANK_PROJECT).forEach(([key, value]) => {
+      setProjects(key, value, uuidv4());
+    });
+  };
+
+  const handleChange = (e, i) => {
+    const { name, value } = e.target;
+    setProjects(name, value, projects[i].id);
+  };
+
+  const handleDelete = (id) => {
+    const projectArray = [...projects];
+    const updatedProjectArray = projectArray.filter(
+      (project) => project.id !== id
+    );
+    setFieldList(updatedProjectArray);
+    localStorage.setItem("projects", JSON.stringify(updatedProjectArray));
+  };
   return (
-    <div>
+    <div className="Projects">
       <h2>Projects</h2>
-      <form className="form">
-        <label htmlFor="title">Title</label>
-        <input type="text" name="title" />
+      {projects.map((val, i) => (
+        <div key={val.id}>
+          <h3>Projects {i + 1}</h3>
 
-        <label htmlFor="link">Link</label>
-        <input type="url" name="link" />
+          <form className="form">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              value={val.title}
+              name="title"
+              onChange={(e) => handleChange(e, i)}
+            />
 
-        <label htmlFor="description">description </label>
-        <input type="textarea" name="description" />
-      </form>
+            <label htmlFor="school">Link</label>
+            <input
+              type="url"
+              value={val.link}
+              name="link"
+              onChange={(e) => handleChange(e, i)}
+            />
+
+            <label htmlFor="description">Description</label>
+            <input
+              type="textarea"
+              value={val.description}
+              name="description"
+              onChange={(e) => handleChange(e, i)}
+            />
+            <button onClick={() => handleDelete(val.id)}>Delete</button>
+          </form>
+        </div>
+      ))}
+      <button onClick={handleClickAdd}>Add</button>
     </div>
   );
 }
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,13 +1,36 @@
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+
 export default function Skills() {
+  const [values, setValues] = useLocalStorage("skills");
+
+  const skills = values.find((item) => item.id === "skills") || {};
+  const { technicalSkills, softSkills } = skills;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues(name, value, "skills");
+  };
+
   return (
     <div>
       <h2>Skills</h2>
       <form className="form">
         <label htmlFor="technicalSkills">Technical skills</label>
-        <input type="text" name="technicalSkills" />
+        <input
+          type="textarea"
+          name="technicalSkills"
+          value={technicalSkills}
+          onChange={handleChange}
+        />
 
         <label htmlFor="softSkills">Soft skills</label>
-        <input type="text" name="softSkills" />
+        <input
+          type="textarea"
+          name="softSkills"
+          value={softSkills}
+          onChange={handleChange}
+        />
       </form>
     </div>
   );

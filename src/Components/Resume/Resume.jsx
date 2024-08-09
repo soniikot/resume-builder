604751ba9842e8afc8e3eea1829a12316f6acb24
soniikot <soniikot@gmail.com>
2024-08-09 14:21:29 -0700

    print button

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -18,83 +18,87 @@ export default function Resume({ setCurrentPage }) {
   };
 
   return (
-    <div className="resume">
-      <button onClick={print()}> Print or Save as PDF</button>
-      <h1>{position}</h1>
-      <h2>
-        {firstName} {lastName}
-      </h2>
-
-      <div className="general-fields">
-        <h3>Email: {email}</h3>
-        <h3>Phone: {phone}</h3>
-        <h3>Address: {address}</h3>
-
-        <button className="edit-button" onClick={() => handleEdit(0)}>
-          Edit
-        </button>
-      </div>
-      <h2>Experience</h2>
-      <div className="multifields">
-        {job &&
-          job.map((job, idx) => (
-            <div className="fields" key={idx}>
-              <h3>Job Title: {job.jobTitle}</h3>
-              <p>
-                {job.dateStarted} - {job.dateFinished}
-              </p>
-              <p>Description: {job.description}</p>
-            </div>
-          ))}
-        <button className="edit-button" onClick={() => handleEdit(1)}>
-          Edit
-        </button>
-      </div>
-
-      <h2>Education</h2>
-      <div className="multifields">
-        {schools &&
-          schools.map((school, idx) => (
-            <div className="fields" key={idx}>
-              <h3>Degree: {school.degree}</h3>
-              <p>School Name: {school.school}</p>
-              <p>Date Finished: {school.dateFinished}</p>
-            </div>
-          ))}
-        <button className="edit-button" onClick={() => handleEdit(2)}>
-          Edit
-        </button>
-      </div>
-
-      <h2>Projects</h2>
-      <div className="multifields">
-        {projects &&
-          projects.map((project, idx) => (
-            <div className="fields" key={idx}>
-              <h3>Title: {project.title}</h3>
-              <p>
-                Link: <a href={project.link}> {project.link}</a>
-              </p>
-              <p>
-                {" "}
-                <b>Description:</b>
-                {project.description}
-              </p>
-            </div>
-          ))}
-        <button className="edit-button" onClick={() => handleEdit(3)}>
-          Edit
-        </button>
-      </div>
-
-      <div className="general-fields">
-        <h4>Technical Skills:</h4> <p>{technicalSkills}</p>
-        <h4>Soft Skills:</h4> <p>{softSkills}</p>
-      </div>
-
-      <button className="edit-button" onClick={() => handleEdit(4)}>
-        edit
+    <>
+      <button class="print-button" onClick={() => print()}>
+        {" "}
+        Print or Save as PDF
       </button>
-    </div>
+      <div className="resume">
+        <h1>{position}</h1>
+        <h2>
+          {firstName} {lastName}
+        </h2>
+
+        <div className="general-fields">
+          <h3>Email: {email}</h3>
+          <h3>Phone: {phone}</h3>
+          <h3>Address: {address}</h3>
+
+          <button className="edit-button" onClick={() => handleEdit(0)}>
+            Edit
+          </button>
+        </div>
+        <h2>Experience</h2>
+        <div className="multifields">
+          {job &&
+            job.map((job, idx) => (
+              <div className="fields" key={idx}>
+                <h3>Job Title: {job.jobTitle}</h3>
+                <p>
+                  {job.dateStarted} - {job.dateFinished}
+                </p>
+                <p>Description: {job.description}</p>
+              </div>
+            ))}
+          <button className="edit-button" onClick={() => handleEdit(1)}>
+            Edit
+          </button>
+        </div>
+
+        <h2>Education</h2>
+        <div className="multifields">
+          {schools &&
+            schools.map((school, idx) => (
+              <div className="fields" key={idx}>
+                <h3>Degree: {school.degree}</h3>
+                <p>School Name: {school.school}</p>
+                <p>Date Finished: {school.dateFinished}</p>
+              </div>
+            ))}
+          <button className="edit-button" onClick={() => handleEdit(2)}>
+            Edit
+          </button>
+        </div>
+
+        <h2>Projects</h2>
+        <div className="multifields">
+          {projects &&
+            projects.map((project, idx) => (
+              <div className="fields" key={idx}>
+                <h3>Title: {project.title}</h3>
+                <p>
+                  Link: <a href={project.link}> {project.link}</a>
+                </p>
+                <p>
+                  {" "}
+                  <b>Description:</b>
+                  {project.description}
+                </p>
+              </div>
+            ))}
+          <button className="edit-button" onClick={() => handleEdit(3)}>
+            Edit
+          </button>
+        </div>
+
+        <div className="general-fields">
+          <h4>Technical Skills:</h4> <p>{technicalSkills}</p>
+          <h4>Soft Skills:</h4> <p>{softSkills}</p>
+          <button className="edit-button" onClick={() => handleEdit(4)}>
+            Edit
+          </button>
+        </div>
+      </div>
+    </>
   );
 }

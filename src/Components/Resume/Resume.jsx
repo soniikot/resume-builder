e53f7930c4f434d5e0924aada9ce04ca3873e4aa
soniikot <soniikot@gmail.com>
2024-08-09 17:00:29 -0700

    not showing sections if arrays are empty

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -51,68 +51,81 @@ export default function Resume({ setCurrentPage }) {
           </div>
         </div>
 
-        <h2>Experience</h2>
-        <div className="fields">
-          <div className="fields__list">
-            {job &&
-              job.map((job, idx) => (
-                <div key={idx}>
-                  <h3>Job Title: {job.jobTitle}</h3>
-                  <p>
-                    {job.dateStarted} - {job.dateFinished}
-                  </p>
-                  <p>Description: {job.description}</p>
-                </div>
-              ))}
-          </div>
-          <div>
-            <button className="edit-button" onClick={() => handleEdit(1)}>
-              Edit
-            </button>
-          </div>
-        </div>
+        {job.length > 0 ? (
+          <>
+            <h2>Experience</h2>
+            <div className="fields">
+              <div className="fields__list">
+                {job &&
+                  job.map((job, idx) => (
+                    <div key={idx}>
+                      <h3>Job Title: {job.jobTitle}</h3>
+                      <p>
+                        {job.dateStarted} - {job.dateFinished}
+                      </p>
+                      <p>Description: {job.description}</p>
+                    </div>
+                  ))}
+              </div>
+              <div>
+                <button className="edit-button" onClick={() => handleEdit(1)}>
+                  Edit
+                </button>
+              </div>
+            </div>
+          </>
+        ) : null}
 
-        <h2>Education</h2>
-        <div className="fields">
-          <div className="fields__list">
-            {schools &&
-              schools.map((school, idx) => (
-                <div key={idx}>
-                  <h3>Degree: {school.degree}</h3>
-                  <p>School Name: {school.school}</p>
-                </div>
-              ))}
-          </div>
-          <div className=" fields__button">
-            <button className="edit-button" onClick={() => handleEdit(2)}>
-              Edit
-            </button>
-          </div>
-        </div>
+        {schools.length > 0 ? (
+          <>
+            <h2>Education</h2>
+            <div className="fields">
+              <div className="fields__list">
+                {schools &&
+                  schools.map((school, idx) => (
+                    <div key={idx}>
+                      <h3>Degree: {school.degree}</h3>
+                      <p>School Name: {school.school}</p>
+                    </div>
+                  ))}
+              </div>
+              <div className=" fields__button">
+                <button className="edit-button" onClick={() => handleEdit(2)}>
+                  Edit
+                </button>
+              </div>
+            </div>
+          </>
+        ) : null}
 
-        <h2>Projects</h2>
-        <div className="fields fields--projects">
-          <div className="fields__list">
-            {projects &&
-              projects.map((project, idx) => (
-                <div key={idx}>
-                  <h3>Title: {project.title}</h3>
-                  <p>
-                    Link: <a href={project.link}> {project.link}</a>
-                  </p>
-                  <p>
-                    <b>Description:</b>
-                    {project.description}
-                  </p>
-                </div>
-              ))}
-          </div>
-          <div>
-            <button className="edit-button" onClick={() => handleEdit(3)}>
-              Edit
-            </button>
-          </div>
-        </div>
+        {projects.length > 0 ? (
+          <>
+            <h2>Projects</h2>
+
+            <div className="fields fields--projects">
+              <div className="fields__list">
+                {projects &&
+                  projects.map((project, idx) => (
+                    <div key={idx}>
+                      <h3>Title: {project.title}</h3>
+                      <p>
+                        Link: <a href={project.link}> {project.link}</a>
+                      </p>
+                      <p>
+                        <b>Description:</b>
+                        {project.description}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+              <div>
+                <button className="edit-button" onClick={() => handleEdit(3)}>
+                  Edit
+                </button>
+              </div>
+            </div>
+          </>
+        ) : null}
       </div>
     </>
   );
